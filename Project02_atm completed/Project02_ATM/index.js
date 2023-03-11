22bb5f871d60fd31299b17a4cce207781ef06940
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2023-03-11 22:32:38 +0500

    Add files via upload

---
+++ Project02_atm completed/Project02_ATM/index.js
@@ -0,0 +1,58 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "Kindly enter your Id: "
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly enter your PIN: "
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving", "Default"],
+        message: "Select your account type: "
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdraw"],
+        message: "Select your transaction type: ",
+        when(answers) {
+            return answers.accountType;
+        }
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000],
+        message: "Select your amount: ",
+        when(answers) {
+            return answers.transactionType === "Fast Cash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount: ",
+        when(answers) {
+            return answers.transactionType === "Withdraw";
+        }
+    },
+]);
+if (answers.userId && answers.userPin) {
+    const balance = Math.floor(Math.random() * 10000000);
+    console.log(balance);
+    const EnteredAmount = answers.amount;
+    if (balance >= EnteredAmount) {
+        const remaining = balance - EnteredAmount;
+        console.log("Your remaining balance is ", remaining);
+    }
+    else {
+        console.log("Insufficient Balance");
+    }
+}
+;
